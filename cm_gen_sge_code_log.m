e84c56fc706f73430f2763297a6f0862264c27c9
Sang Phan <plsang@nii.ac.jp>
2015-03-31 22:16:22 +0900

    supports more dubug option: debug 0 (no error, no output), 1 (error only), 2 (debug only), 3 (both)

--- cm_gen_sge_code_log.m
+++ cm_gen_sge_code_log.m
@@ -1,11 +1,34 @@
-function gen_sge_code_log(script_name, pattern, total_segments, num_job, pe)
+function cm_gen_sge_code_log(script_name, pattern, total_segments, num_job, varargin)
 	
+    debug = 0;
+    pe = 0;
+    
+    if nargin < 4,
+        fprintf('Usage: cm_gen_sge_code_log(script_name, pattern, total_segments, num_job, varargin) \n');
+        fprintf(' varargin: debug 0 (no error, no output), 1 (error only), 2 (debug only), 3 (both) ) \n');
+        fprintf(' varargin: pe 0 (number of local slots per job) ) \n');
+        return;
+    end
+    
+    for k=1:2:length(varargin),
+        opt = lower(varargin{k});
+        arg = varargin{k+1} ;
+      
+        switch opt
+            case 'debug'
+                debug = arg;
+            case 'pe' ;
+                pe = arg ;
+            otherwise
+                error(sprintf('Option ''%s'' unknown.', opt)) ;
+        end  
+    end
+
 	configs = set_global_config();
 	
 	script_dir = configs.sge_script_dir;
 	
 	sge_sh_file = sprintf('%s/%s.sh', script_dir, script_name);
-	
 	
 	[file_dir, file_name] = fileparts(sge_sh_file);
 	output_dir = [script_dir, '/', script_name];
@@ -13,7 +36,14 @@ function gen_sge_code_log(script_name, pattern, total_segments, num_job, pe)
 	if exist(output_dir, 'file') ~= 7,
 		mkdir(output_dir);
 	end
-	
+
+    if debug > 0,
+        debug_dir = sprintf('%s/debug', output_dir);
+        if exist(debug_dir, 'file') ~= 7,
+            mkdir(debug_dir);
+            change_perm(debug_dir);
+        end
+    end
 
 	output_file = sprintf('%s/%s.qsub.sh', output_dir, file_name);
 	fh = fopen(output_file, 'w');
@@ -30,12 +60,30 @@ function gen_sge_code_log(script_name, pattern, total_segments, num_job, pe)
 		
 		params = sprintf(pattern, start_idx, end_idx);
 		
-        if ~exist('pe', 'var'),
-			fprintf(fh, 'qsub -e /dev/null -o /dev/null %s %s\n', sge_sh_file, params);
-		else
-			fprintf(fh, 'qsub -pe localslots %d -e /dev/null -o /dev/null %s %s\n', pe, sge_sh_file, params);
-		end
-
+        cmd = 'qsub';
+        if pe == 1,
+            cmd = sprintf('%s -pe localslots %d ', cmd, pe);
+        end
+        
+        switch debug,
+            case 1
+                e_file = sprintf('%s/%s.error.s%06d_e%06d.log', debug_dir, script_name, start_idx, end_idx);
+                o_file = '/dev/null';
+            case 2
+                e_file = '/dev/null'; 
+                o_file = sprintf('%s/%s.output.s%06d_e%06d.log', debug_dir, script_name, start_idx, end_idx);
+            case 3
+                e_file = sprintf('%s/%s.error.s%06d_e%06d.log', debug_dir, script_name, start_idx, end_idx);
+                o_file = sprintf('%s/%s.output.s%06d_e%06d.log', debug_dir, script_name, start_idx, end_idx);
+            otherwise
+                e_file = '/dev/null'; 
+                o_file = '/dev/null';
+        end
+		
+        cmd = sprintf('%s -e %s -o %s %s %s', cmd, e_file, o_file, sge_sh_file, params);
+        
+        fprintf(fh, '%s\n', cmd);
+        
 		start_idx = end_idx+1;
 	end
 	
