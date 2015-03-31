7df7a492fc66cc08c4b8c75e5adbd4816041c3d1
Sang Phan <plsang@nii.ac.jp>
2015-03-31 16:36:28 +0900

    test new idt

--- popen/popen_dt/popen_dt.m
+++ popen/popen_dt/popen_dt.m
@@ -1,10 +1,14 @@
 
 % Setup constants for example
-filename = '/net/per900a/raid0/plsang/dataset/MED11_Resized/MED11DEV/HVC000965.mp4'; %HVC180809.mp4
+% filename = '/net/per900a/raid0/plsang/dataset/MED11_Resized/MED11DEV/HVC000965.mp4'; %HVC180809.mp4
+filename = '/net/per610a/export/das11f/plsang/codes/opensource/improved_trajectory_release/test_sequences/person01_boxing_d1_uncomp.avi';
 
 % Set up the mpeg audio decode command as a readable stream
-cmd = ['/net/per900a/raid0/plsang/tools/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -G 1'];
+% cmd = ['/net/per900a/raid0/plsang/tools/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -G 1'];
 %cmd = ['/home/plsang/code/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -S 5154 -E 5184 -G 1'];
+bin = 'LD_PRELOAD=/net/per900a/raid0/plsang/usr.local/lib/libstdc++.so /net/per610a/export/das11f/plsang/codes/opensource/improved_trajectory_release/release/DenseTrackStab_HOGHOFMBH';
+
+cmd = sprintf('%s %s', bin, filename);
 
 p = popenr(cmd);
 
@@ -19,7 +23,7 @@ tic
 while true,
 
   % Get the next chunk of data from the process
-  Y = popenr(p, 199, 'float');
+  Y = popenr(p, 396, 'float');
   
   if isempty(Y), break; end;
   
