a36a731a8968848c94159a81f9ed994beb929a43
Sang Phan <plsang@nii.ac.jp>
2015-04-01 12:46:13 +0900

    read block of elements

--- popen/popen_dt/popen_dt.m
+++ popen/popen_dt/popen_dt.m
@@ -1,39 +1,43 @@
+function X = popen_dt()
 
-% Setup constants for example
-% filename = '/net/per900a/raid0/plsang/dataset/MED11_Resized/MED11DEV/HVC000965.mp4'; %HVC180809.mp4
-filename = '/net/per610a/export/das11f/plsang/codes/opensource/improved_trajectory_release/test_sequences/person01_boxing_d1_uncomp.avi';
+    % Setup constants for example
+    % filename = '/net/per900a/raid0/plsang/dataset/MED11_Resized/MED11DEV/HVC000965.mp4'; %HVC180809.mp4
+    filename = '/net/per610a/export/das11f/plsang/codes/opensource/improved_trajectory_release/test_sequences/person01_boxing_d1_uncomp.avi';
 
-% Set up the mpeg audio decode command as a readable stream
-% cmd = ['/net/per900a/raid0/plsang/tools/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -G 1'];
-%cmd = ['/home/plsang/code/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -S 5154 -E 5184 -G 1'];
-bin = 'LD_PRELOAD=/net/per900a/raid0/plsang/usr.local/lib/libstdc++.so /net/per610a/export/das11f/plsang/codes/opensource/improved_trajectory_release/release/DenseTrackStab_HOGHOFMBH';
+    % Set up the mpeg audio decode command as a readable stream
+    % cmd = ['/net/per900a/raid0/plsang/tools/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -G 1'];
+    %cmd = ['/home/plsang/code/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -S 5154 -E 5184 -G 1'];
+    bin = 'LD_PRELOAD=/net/per900a/raid0/plsang/usr.local/lib/libstdc++.so /net/per610a/export/das11f/plsang/codes/opensource/improved_trajectory_release/release/DenseTrackStab_HOGHOFMBH';
 
-cmd = sprintf('%s %s', bin, filename);
+    cmd = sprintf('%s %s', bin, filename);
 
-p = popenr(cmd);
+    p = popenr(cmd);
 
-if p < 0
-  error(['Error running popenr(', cmd,')']);
+    if p < 0
+      error(['Error running popenr(', cmd,')']);
+    end
+
+    BLOCK_SIZE=20000;
+
+    X = zeros(396, BLOCK_SIZE, 'single');
+    ptr = 1;
+
+    while true,
+
+      % Get the next chunk of data from the process
+      Y = popenr(p, 396, 'float');
+      
+      if isempty(Y), break; end;
+      
+      X(:, ptr) = Y;
+      ptr = ptr + 1;
+      
+    end
+
+    X(:, ptr:end) = [];
+
+    % Close pipe
+    popenr(p, -1);
+
 end
-
-X = [];
-
-tic
-
-while true,
-
-  % Get the next chunk of data from the process
-  Y = popenr(p, 396, 'float');
-  
-  if isempty(Y), break; end;
-  
-  X = [X Y];
-  
-end
-
-toc
-
-% Close pipe
-popenr(p, -1);
-
 
--- popen/popen_dt/popen_full.m
+++ popen/popen_dt/popen_full.m
@@ -1,35 +1,50 @@
+function X = popen_full()
 
-% Setup constants for example
-filename = '/net/per900a/raid0/plsang/dataset/MED11_Resized/MED11DEV/HVC000965.mp4'; %HVC180809.mp4
+    filename = '/net/per610a/export/das11f/plsang/codes/opensource/improved_trajectory_release/test_sequences/person01_boxing_d1_uncomp.avi';
 
-% Set up the mpeg audio decode command as a readable stream
-cmd = ['/net/per900a/raid0/plsang/tools/dense_trajectory_release/release/DenseTrack_FULL ', filename, ' -G 1'];
-%cmd = ['/home/plsang/code/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -S 5154 -E 5184 -G 1'];
+    % Set up the mpeg audio decode command as a readable stream
+    % cmd = ['/net/per900a/raid0/plsang/tools/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -G 1'];
+    %cmd = ['/home/plsang/code/dense_trajectory_release/release/DenseTrack_MBH ', filename, ' -S 5154 -E 5184 -G 1'];
+    bin = 'LD_PRELOAD=/net/per900a/raid0/plsang/usr.local/lib/libstdc++.so /net/per610a/export/das11f/plsang/codes/opensource/improved_trajectory_release/release/DenseTrackStab_HOGHOFMBH';
 
-p = popenr(cmd);
+    cmd = sprintf('%s %s', bin, filename);
 
-if p < 0
-  error(['Error running popenr(', cmd,')']);
+    p = popenr(cmd);
+
+    if p < 0
+      error(['Error running popenr(', cmd,')']);
+    end
+
+    BLOCK_SIZE=20000;
+
+    fulldim = 396;
+    X = zeros(fulldim, BLOCK_SIZE, 'single');
+    ptr = 1;
+    
+    pblock = 5000;
+    numel = pblock * fulldim;
+    
+    while true,
+
+      % Get the next chunk of data from the process
+      Y = popenr(p, numel, 'float');
+      
+      if size(Y, 1) ~= numel,
+        ncol = size(Y, 1)/fulldim;
+        X(:, ptr:ptr+ncol-1) = reshape(Y, fulldim, ncol);
+        ptr = ptr + ncol;
+        break;
+      else
+        X(:, ptr:ptr + pblock - 1) = reshape(Y, fulldim, pblock);
+        ptr = ptr + pblock;
+      end
+      
+    end
+
+    X(:, ptr:end) = [];
+
+    % Close pipe
+    popenr(p, -1);
+
 end
-
-X = [];
-
-tic
-
-while true,
-
-  % Get the next chunk of data from the process
-  Y = popenr(p, 433, 'float');
-  
-  if isempty(Y), break; end;
-  
-  X = [X Y];
-  
-end
-
-toc
-
-% Close pipe
-popenr(p, -1);
-
 
